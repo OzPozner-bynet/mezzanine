7b72da12b3844af2d3fa044fa87066a0f3b9d5c2
Oz Pozner <OzP@bynet.co.il>
2024-06-24 08:27:44 +0300

    initial

---
+++ cloudhome/local_settings.py
@@ -0,0 +1,28 @@
+# This file is exec'd from settings.py, so it has access to and can
+# modify all the variables in settings.py.
+
+DEBUG = True
+
+# Make these unique, and don't share it with anybody.
+SECRET_KEY = "django-insecure-*z0wfb5kfd4!jw5kc7vvmcq_#(1hidqky)_ecf=s9i)aj=a-7v"
+NEVERCACHE_KEY = "h7$ja=ir$%l0jr_ozt2x&900=40r_0^q$5yrrv270l(^-(377a"
+
+DATABASES = {
+    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.sqlite3",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "dev.db",
+        # Not used with sqlite3.
+        "USER": "",
+        # Not used with sqlite3.
+        "PASSWORD": "",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+    }
+}
+
+# Allowed development hosts
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "::1"]
---
+++ push.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git push --set-upstream git@github.com:OzPozner-bynet/mezzanine.git master
